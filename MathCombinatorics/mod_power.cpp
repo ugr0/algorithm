aa59da3e6de52864acbcd0192f66e41d1f9c3a41
uglo1 <yujiro@sein.mse.kyutech.ac.jp>
2020-07-23 23:35:56 +0900

    add mod_power

---
+++ MathCombinatorics/mod_power.cpp
@@ -0,0 +1,23 @@
+//
+// 累乗　a^n mod. m   O(log√n)
+//
+
+
+#include <iostream>
+using namespace std;
+
+
+long long modpow(long long a, long long n, long long mod) {
+    long long res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+
+int main() {
+    cout << modpow(3, 45, 1000000007) << endl;
+}
