137bf1a4983fabb531a1da8125c7220475c7ef9d
uglo1 <yujiro@sein.mse.kyutech.ac.jp>
2020-07-25 17:14:16 +0900

    add count_linking_element_bfs

---
+++ GraphTheory/count_linking_element_bfs.cpp
@@ -0,0 +1,43 @@
+//
+//連結とは限らない無向グラフが何個の連結成分からなるかを数える
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int main() {
+    // 頂点数と辺数
+    int N, M; cin >> N >> M;
+
+    // グラフ入力受取
+    Graph G(N);
+    for (int i = 0; i < M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    // 頂点 s をスタートとした探索
+    vector<int> dist(N, -1);
+    queue<int> que;
+    int count = 0;
+    for (int v = 0; v < N; ++v) {
+        if (dist[v] != -1) continue; // v が探索済みならスルー
+        dist[v] = 0, que.push(v);
+        while (!que.empty()) {
+            int v = que.front(); que.pop();
+            for (auto nv : G[v]) {
+                if (dist[nv] == -1) {
+                    dist[nv] = dist[v] + 1;
+                    que.push(nv);
+                }
+            }
+        }
+        ++count;
+    }
+    cout << count << endl;
+}
