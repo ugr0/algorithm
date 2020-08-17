e85cd1cd7d087fda7347696230bd3cf749f1552d
uglo1 <yujiro@sein.mse.kyutech.ac.jp>
2020-08-17 12:22:32 +0900

    a

---
+++ GraphTheory/dijkstra.cpp
@@ -0,0 +1,88 @@
+//
+// dijkstra　O(E log(V))
+// https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_f
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+using ll = long long;
+const ll INF = (1LL << 60);
+
+struct edge{ll to, cost;};
+typedef pair<ll,ll> P;
+struct graph{
+    ll V;
+    vector<vector<edge> > G;
+    vector<ll> d;
+
+    graph(ll n){
+        init(n);
+    }
+
+    void init(ll n){
+        V = n;
+        G.resize(V);
+        d.resize(V);
+        rep(i,V){
+        d[i] = INF;
+        }
+    }
+
+    void add_edge(ll s, ll t, ll cost){
+        edge e;
+        e.to = t, e.cost = cost;
+        G[s].push_back(e);
+    }
+
+    void dijkstra(ll s){
+        rep(i,V) d[i] = INF;
+        d[s] = 0;
+        priority_queue<P,vector<P>, greater<P> > que;
+        que.push(P(0,s));
+        while(!que.empty()){
+            P p = que.top(); que.pop();
+            ll v = p.second;
+            if(d[v]<p.first) continue;
+            for(auto e : G[v]){
+                if(d[e.to]>d[v]+e.cost){
+                    d[e.to] = d[v]+e.cost;
+                    que.push(P(d[e.to],e.to));
+                }
+            }
+        }
+    }
+};
+
+/********** Main()  function **********/
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, k; cin >> n >> k;
+    //n頂点のグラフを作成
+    graph g(n);
+
+    rep(i,k){
+        int x; cin >> x;
+        if(x==0){
+            int a,b; cin >> a >> b;
+            a--,b--;
+            //頂点aからの各頂点への単一視点最短距離を探索できる
+            g.dijkstra(a);
+
+            //g.d[i]でaから頂点iへの最短距離が計算される
+            //もし経路がないならINFが返る
+            if(g.d[b]==INF) cout << -1 << endl;
+            else cout << g.d[b] << endl;
+        }else{
+            int c,d,e; cin >> c >> d >> e;
+            c--,d--;
+            //g.add_edge(a,b,cost)..aからbにコストcostの辺が張られる
+            g.add_edge(c,d,e);
+            g.add_edge(d,c,e);
+        }
+    }
+
+}
+/********  Main() Ends Here *************/
--- MathNumberTheory/Eratosthenes.cpp
+++ MathNumberTheory/Eratosthenes.cpp
@@ -4,9 +4,9 @@
 
 
 /*
- 
+
     n 以下の素数をすべて列挙する
- 
+
 */
 
 
@@ -15,25 +15,27 @@
 #include <algorithm>
 using namespace std;
 
+// isprime[i] iが素数なら1,素数でないなら0
+vector<int> isprime;
 
-vector<bool> isprime;
 vector<int> Era(int n) {
-    isprime.resize(n, true);
+    isprime.resize(n, 1);
     vector<int> res;
-    isprime[0] = false; isprime[1] = false;
-    for (int i = 2; i < n; ++i) isprime[i] = true;
+    isprime[0] = 0; isprime[1] = 0;
+    for (int i = 2; i < n; ++i) isprime[i] = 1;
     for (int i = 2; i < n; ++i) {
         if (isprime[i]) {
             res.push_back(i);
-            for (int j = i*2; j < n; j += i) isprime[j] = false;
+            for (int j = i*2; j < n; j += i) isprime[j] = 0;
         }
     }
     return res;
 }
 
+//primes...100000未満の素数の配列
+vector<int> primes = Era(100000);
 
 int main() {
-    vector<int> primes = Era(1000000);
     int n;
     while (cin >> n) {
         int num = upper_bound(primes.begin(), primes.end(), n) - primes.begin(); // n 以下が何個か
