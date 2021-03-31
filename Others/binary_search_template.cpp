de6319a11b9bb68132b874297f7c07771026598f
uglo1 <yujiro.takahashi0912@gmail.com>
2021-03-31 13:57:20 +0900

    ♻️ refactor code

--- Others/binary_search_template.cpp
+++ Others/binary_search_template.cpp
@@ -1,33 +1,30 @@
-/*
-二分探索めぐる式
-
-入力: a(n), key
-出力: keyがどこに入るか
-*/
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main(){
-    int n, key; cin >> n >> key;
-    vector<int> a(n);
-    for(int i=0; i<n; i++) cin >> a[i];
+int main() {
+  int n, key;
+  cin >> n >> key;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) cin >> a[i];
 
-    int ng = -1;
-    int ok = (int)a.size();
+  int ng = -1;
+  int ok = (int)a.size();
 
-    while(abs(ok - ng) > 1){
-        int mid = (ng + ok) / 2;
-        auto f = [&](int idx){
-            //条件判定が>=であればlower_bound、>であればupper_bound
-            if(a[idx]>= key) return true;
-            else return false;
-        };
+  while (abs(ok - ng) > 1) {
+    int mid = (ng + ok) / 2;
+    auto f = [&](int idx) {
+      if (a[idx] >= key)
+        return true;
+      else
+        return false;
+    };
 
-        if(f(mid)) ok = mid;
-        else ng = mid;
-    }
+    if (f(mid))
+      ok = mid;
+    else
+      ng = mid;
+  }
 
-    cout << ok << endl;
+  cout << ok << endl;
 }
