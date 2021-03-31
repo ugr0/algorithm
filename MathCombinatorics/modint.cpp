3b3df5f3a33c2d6cd2ca248b1b3a4a8aa11615bb
uglo1 <yujiro@sein.mse.kyutech.ac.jp>
2021-03-31 13:47:24 +0900

    🎉 first commit

---
+++ MathCombinatorics/modint.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+    long long x;
+    mint(long long x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(long long t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+// nCa
+mint choose(int n, int a){
+    mint x = 1, y = 1;
+    for(int i=0; i<a; i++){
+        x *= n-i;
+        y *= i+1;
+    }
+
+    return x / y;
+}
